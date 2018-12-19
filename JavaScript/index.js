b451a86d79bd449ad4389e6055b47aa5f3905b5e
Eddie Fabre <EddieNFab@Gmail.com>
2018-12-19 07:24:26 -0500

    Minor

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,8 +1,6 @@
-
 document.getElementById("lbsinput").addEventListener('input', function(e){
     let lbs = e.target.value;
-    document.getElementById("gramsoutput").innerHTML = lbs / 0.0022046;
+    document.getElementById("gramsoutput").innerHTML = lbs / 0.002204;
     document.getElementById("kilogramsoutput").innerHTML = lbs / 2.2046;
     document.getElementById("ouncesoutput").innerHTML = lbs * 16;
-    //alert (lbs);
 });
