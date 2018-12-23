032d0385b16bc3b6b9ebd83d0febf4abdb25e637
Eddie Fabre <EddieNFab@Gmail.com>
2018-12-23 12:43:18 -0500

    Minor Updates

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -8,22 +8,28 @@ document.getElementById("lbsinput").addEventListener('input', function(e){
 */
 
 $(document).ready(function () {
-    
+
     /************************
     ON FIRST BUTTON CLICK    
     ************************/
     $("#firstbutton").click(function () {
-        
-        var currentmu = $("#metricinput").attr("placeholder");
-        
-        if ( $("#firstbutton").html = "G" ) {
-            if (currentmu = "enter weight in pounds..."){
-                
-            }
-        } else if ( $("#firstbutton").html = "G" ) {
-            
+
+        var currentmu = $("#metricinput").attr("placeholder"); //CREATE VARIABLE EQUAL TO THE CURRENT INPUT'S METRIC
+
+        if ($("#firstbutton").text = "G") { // IF THE FIRST BUTTON CLICKED IS GRAMS 
+
+            $("#metricinput").attr("placeholder", "enter weight in grams..."); //CHANGE UNIT TO GRAMS
+            $("#firstbutton").html("LB"); //CHANGE BUTTON TO POUNDS
+            $("#firstheading").html("Pounds:"); //CHANGE CARD HEADING TO POUNDS
+
+        } else if ($("#firstbutton").text = "LB") { // IF THE FIRST BUTTON CLICKED IS POUNDS
+
+            $("#metricinput").attr("placeholder", "enter weight in pounds..."); //CHANGE UNIT TO POUNDS
+            $("#firstbutton").html("G"); //CHANGE BUTTON TO GRAMS
+            $("#firstheading").html("Grams:"); //CHANGE CARD HEADING TO GRAMS
+
         }
-        
+
         /*
         if ($("#metricinput").attr("placeholder") == "enter weight in pounds...") {// IF THE CURRENT UNIT IS POUNDS
             $("#metricinput").attr("placeholder", "enter weight in grams...");// CHANGE UNIT TO GRAMS
@@ -34,7 +40,7 @@ $(document).ready(function () {
             $("#firstbutton").html("G");
             $("#firstheading").html("Grams");
         }*/
-        
+
     });
 
 });
--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 1.Style Buttons
 2. set width on buttons for all screen sizes
 3.Set Javascript for buttons when pressed
+
+
+ON FIRST BUTTON CLICK
+
+1.CREATE A VARIABLE EQUAL TO WHATEVER THE CURRENT INPUT METRIC IS
+    
+    **IF FIRST BUTTON IS G** 
