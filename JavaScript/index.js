af896e22314da5bdfcbeaf3175d24cf15b7a4537
Eddie Fabre <EddieNFab@Gmail.com>
2018-12-23 20:37:32 -0500

    Update index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -6,41 +6,55 @@ document.getElementById("lbsinput").addEventListener('input', function(e){
     document.getElementById("ouncesoutput").innerHTML = lbs * 16;
 });
 */
-
+/*************
+ON DOCLOAD    
+**************/
 $(document).ready(function () {
 
     /************************
     ON FIRST BUTTON CLICK    
     ************************/
     $("#firstbutton").click(function () {
+        var original;
 
-        var currentmu = $("#metricinput").attr("placeholder"); //CREATE VARIABLE EQUAL TO THE CURRENT INPUT'S METRIC
+        var whichbutton = ($(this).text = "G") ? "LB" : "G";
 
-        if ($("#firstbutton").text = "G") { // IF THE FIRST BUTTON CLICKED IS GRAMS 
-
+        function inputchangegram() {
             $("#metricinput").attr("placeholder", "enter weight in grams..."); //CHANGE UNIT TO GRAMS
-            $("#firstbutton").html("LB"); //CHANGE BUTTON TO POUNDS
+            $("#firstbutton").text(whichbutton); //CHANGE BUTTON TO POUNDS
             $("#firstheading").html("Pounds:"); //CHANGE CARD HEADING TO POUNDS
-
-        } else if ($("#firstbutton").text = "LB") { // IF THE FIRST BUTTON CLICKED IS POUNDS
-
-            $("#metricinput").attr("placeholder", "enter weight in pounds..."); //CHANGE UNIT TO POUNDS
-            $("#firstbutton").html("G"); //CHANGE BUTTON TO GRAMS
-            $("#firstheading").html("Grams:"); //CHANGE CARD HEADING TO GRAMS
-
+            var original = false;
         }
 
+        function inputchangepound() {
+            $("#metricinput").attr("placeholder", "enter weight in pounds..."); //CHANGE UNIT TO POUNDS        
+            $("#firstbutton").text(whichbutton); //CHANGE BUTTON TO GRAMS
+            $("#firstheading").html("Grams:"); //CHANGE CARD HEADING TO GRAMS
+            var orginial = true;
+        }
+
+        ($(this).text = "G") ? inputchangegram() : inputchangepound();
         /*
-        if ($("#metricinput").attr("placeholder") == "enter weight in pounds...") {// IF THE CURRENT UNIT IS POUNDS
-            $("#metricinput").attr("placeholder", "enter weight in grams...");// CHANGE UNIT TO GRAMS
-            $("#firstbutton").html("LB");//CHANGE BUTTON TO POUNDS
-            $("#firstheading").html("Pounds");//CHANGE CARD HEADING TO POUNDS
-        } else {
-            $("#metricinput").attr("placeholder", "enter weight in pounds...");
-            $("#firstbutton").html("G");
-            $("#firstheading").html("Grams");
+        if ($(this).text = "G") {
+            $(this).text = ($(this).text = "G") ? inputchangegram() : inputchangepound();
+        } else if ($(this).text = "LB") {
+            $(this).text = ($(this).text = "G") ? inputchangegram() : inputchangepound();
         }*/
 
+        //var currentmu = $("#metricinput").attr("placeholder"); //CREATE VARIABLE EQUAL TO THE CURRENT INPUT'S METRIC
+
+
+        /*  if ($(this).text = "G") { // IF THE FIRST BUTTON CLICKED IS GRAMS
+              $("#metricinput").attr("placeholder", "enter weight in grams..."); //CHANGE UNIT TO GRAMS
+              $("#firstbutton").text(whichbutton); //CHANGE BUTTON TO POUNDS
+              $("#firstheading").html("Pounds:"); //CHANGE CARD HEADING TO POUNDS
+          }         
+          
+          else if (fake = false) {
+              $("#metricinput").attr("placeholder", "enter weight in pounds..."); //CHANGE UNIT TO POUNDS
+              $("#firstbutton").text(whichbutton); //CHANGE BUTTON TO GRAMS
+              $("#firstheading").html("Grams:"); //CHANGE CARD HEADING TO GRAMS
+          } */
     });
 
 });
