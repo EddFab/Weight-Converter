64654c94ed5a69fbe6a57e8c763c9d3a80d94850
Eddie Fabre <EddieNFab@Gmail.com>
2018-12-25 23:52:09 -0500

    Finished

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+    $("input").val("");
     $("input").focus();
 
     /************************
@@ -18,29 +18,24 @@ $(document).ready(function () {
 
         var changebutton; //CREATE VARIABLE FOR FUNCTION
         if ($(this).text() == "G") { // IF THE BUTTON PRESSED WAS GRAMS
-
             changebutton = function () { //FUNCTION NAMED "CHANGEBUTTON" EQUALS
                 $("#firstbutton").text("LB"); // CHANGE BUTTON FROM GRAMS TO POUNDS
                 $("#metricinput").attr("placeholder", "enter weight in grams..."); //CHANGE UNIT TO GRAMS
                 $("#firstheading").html("Pounds:"); //CHANGE CARD HEADING TO POUNDS
                 $("input").focus();
             };
-
         } else {
-
             changebutton = function () { //FUNCTION NAMED "CHANGEBUTTON" EQUALS
                 $("#firstbutton").text("G"); // CHANGE BUTTON FROM POUNDS TO GRAMS
                 $("#metricinput").attr("placeholder", "enter weight in pounds..."); //CHANGE UNIT TO POUNDS
                 $("#firstheading").html("Grams:"); //CHANGE CARD HEADING TO GRAMS
                 $("input").focus();
             };
-
         }
         changebutton();
         $("input").focus();
 
     });
-
 
     /************************
     ON SECOND BUTTON CLICK    
@@ -58,23 +53,19 @@ $(document).ready(function () {
 
         var changebutton; //CREATE VARIABLE FOR FUNCTION
         if ($(this).text() == "KG") { // IF THE BUTTON PRESSED WAS KILOGRAMS
-
             changebutton = function () { //FUNCTION NAMED "CHANGEBUTTON" EQUALS
                 $("#secondbutton").text("LB"); // CHANGE BUTTON FROM KILOGRAMS TO POUNDS
                 $("#metricinput").attr("placeholder", "enter weight in kilograms..."); //CHANGE UNIT TO KILOGRAMS
                 $("#secondheading").html("Pounds:"); //CHANGE CARD HEADING TO POUNDS
                 $("input").focus();
             };
-
         } else {
-
             changebutton = function () { //FUNCTION NAMED "CHANGEBUTTON" EQUALS
                 $("#secondbutton").text("KG"); // CHANGE BUTTON FROM POUNDS TO KILOGRAMS
                 $("#metricinput").attr("placeholder", "enter weight in pounds..."); //CHANGE UNIT TO POUNDS
                 $("#secondheading").html("Kilograms:"); //CHANGE CARD HEADING TO KILOGRAMS
                 $("input").focus();
             };
-
         }
         changebutton();
         $("input").focus();
@@ -97,23 +88,19 @@ $(document).ready(function () {
 
         var changebutton; //CREATE VARIABLE FOR FUNCTION
         if ($(this).text() == "OZ") { // IF THE BUTTON PRESSED WAS OUNCES
-
             changebutton = function () { //FUNCTION NAMED "CHANGEBUTTON" EQUALS
                 $("#thirdbutton").text("LB"); // CHANGE BUTTON FROM OUNCES TO POUNDS
                 $("#metricinput").attr("placeholder", "enter weight in ounces..."); //CHANGE UNIT TO OUNCES
                 $("#thirdheading").html("Pounds:"); //CHANGE CARD HEADING TO POUNDS
                 $("input").focus();
             };
-
         } else {
-
             changebutton = function () { //FUNCTION NAMED "CHANGEBUTTON" EQUALS
                 $("#thirdbutton").text("OZ"); // CHANGE BUTTON FROM POUNDS TO KILOGRAMS
                 $("#metricinput").attr("placeholder", "enter weight in pounds..."); //CHANGE UNIT TO POUNDS
                 $("#thirdheading").html("Ounces:"); //CHANGE CARD HEADING TO KILOGRAMS
                 $("input").focus();
             };
-
         }
         changebutton();
         $("input").focus();
@@ -125,25 +112,43 @@ $(document).ready(function () {
     *******************/
     $("input").on("input", function () {
 
+        var changeoutput;
         var numbercheck = function () {
             alert("Please Enter Numeric Values Only");
             $("input").val("");
             $("input").focus();
         };
 
-        ($.isNumeric($(this).val())) ? $("input").focus() : numbercheck();
-
-        var changeoutput;
+        ($.isNumeric($(this).val())) ? $("input").focus(): numbercheck();
 
         if ($("#metricinput").attr("placeholder") == "enter weight in pounds...") {
-
             let output = $(this).val();
             changeoutput = function () {
                 $("#gramsoutput").html((output / 0.002204).toFixed(2));
                 $("#kilogramsoutput").html((output / 2.2046).toFixed(2));
                 $("#ouncesoutput").html((output * 16).toFixed(2));
             }
-
+        } else if ($("#metricinput").attr("placeholder") == "enter weight in grams...") {
+            let output = $(this).val();
+            changeoutput = function () {
+                $("#gramsoutput").html((output * 0.0022046226).toFixed(2));
+                $("#kilogramsoutput").html((output * 0.001).toFixed(2));
+                $("#ouncesoutput").html((output * 0.035273962).toFixed(2));
+            }
+        } else if ($("#metricinput").attr("placeholder") == "enter weight in kilograms...") {
+            let output = $(this).val();
+            changeoutput = function () {
+                $("#gramsoutput").html((output * 1000).toFixed(2));
+                $("#kilogramsoutput").html((output * 2.2046226).toFixed(2));
+                $("#ouncesoutput").html((output * 35.273962).toFixed(2));
+            }
+        } else if ($("#metricinput").attr("placeholder") == "enter weight in ounces...") {
+            let output = $(this).val();
+            changeoutput = function () {
+                $("#gramsoutput").html((output * 28.349523).toFixed(2));
+                $("#kilogramsoutput").html((output * 0.028349523).toFixed(2));
+                $("#ouncesoutput").html((output * 0.0625).toFixed(2));
+            }
         }
         changeoutput();
     });
